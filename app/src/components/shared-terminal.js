0dfbc21e41fd9bdfab7a937232a97a8b686ebe98
adnason <adnason@ncsu.edu>
2022-07-17 21:34:11 -0400

    fixed bug with re-opening terminals

--- app/src/components/shared-terminal.js
+++ app/src/components/shared-terminal.js
@@ -1,26 +1,37 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { io } from 'socket.io-client';
 import { useParams } from 'react-router-dom';
 import { Terminal } from 'xterm';
+import { SerializeAddon } from "xterm-addon-serialize";
 import 'xterm/css/xterm.css'
 
 function SharedTerminal(props) {
+  const [active, setActive] = useState(false)
   const terminalRef = useRef();
   const { room } = useParams();
+
   useEffect(() => {
     let host = window.location.hostname;
     if (host === "localhost") host += ":3001";
-    const s = io(host, { transports: ['websocket'], secure: true });
-    s.emit('join-terminal', room);
+    const socket = io(host, { transports: ['websocket'], secure: true });
+    socket.emit('join-terminal', room)
 
-    const t = new Terminal();
-    t.open(terminalRef.current);
-    s.on('stdo', (v) => {
-      t.write(v);
-    });
-    t.onData(stdin => {
-      s.emit('stdin', stdin);
-    });
+    const terminal = new Terminal()
+    const serializeAddon = new SerializeAddon();
+    terminal.loadAddon(serializeAddon);
+    
+    if (!active) {
+      terminal.open(terminalRef.current) 
+      setActive(true)
+    }
+    
+    terminal.onData(data => {
+      socket.emit('stdin', data)
+    })
+
+    socket.on('stdout', data => {
+      terminal.write(data)
+    })
 
   }, []);
 
--- app/src/components/shared-textarea.js
+++ app/src/components/shared-textarea.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
 import { useParams } from 'react-router-dom';
 
-function SharedTextarea(props) {
+function SharedTextarea() {
   const [value, _setValue] = useState('');
   const [socket, setSocket] = useState();
   const { room } = useParams();
