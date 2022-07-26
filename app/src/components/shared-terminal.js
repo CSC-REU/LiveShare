ac07bb4bfd073922fd1e7878c4939096df22827f
adnason <adnason@ncsu.edu>
2022-07-26 16:34:11 -0400

    removed unused import

--- app/src/components/shared-terminal.js
+++ app/src/components/shared-terminal.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
 import { useParams } from 'react-router-dom';
 import { Terminal } from 'xterm';
