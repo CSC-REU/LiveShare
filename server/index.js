81944231a4f4529195a710c599be8ca544541c1a
adnason <adnason@ncsu.edu>
2022-07-26 22:19:56 -0400

    added changes to server file

--- server/index.js
+++ server/index.js
@@ -6,57 +6,49 @@ const users = {}
 const pty = require('node-pty');
 let terminalPtys = {};
 
+let textareaHistory = ''
+let terminalHistory = ''
+
 io.sockets.on('connection', socket => {
-    socket.on('join-textarea', async (room) => {
+
+    socket.on('join-textarea', async room => {
         socket.leaveAll();
         socket.join(room);
-        console.log(socket.id, 'joining textarea room', room);
-        let justJoinedId = socket.id;
-        let roomsSockets = (await io.in(room).fetchSockets())[0].nsp.sockets;
-        let roomsSocketsIds = Array.from(roomsSockets.keys())
-            .filter(
-                function (e) {
-                    if (e !== justJoinedId) return true;
-                    else return false;
-                });
-        if(roomsSocketsIds.length > 0) {
-            roomsSockets.get(roomsSocketsIds[0]).emit('userconnect', justJoinedId);
-            console.log('filtered array of ids', roomsSocketsIds);
-        }
-        socket.on('existingvalue', ({giveValueToThisId, value}) => {
-            roomsSockets.get(giveValueToThisId).emit('textarea', value);
-            console.log('recieving existing value', value);
-        });
-        socket.on('textarea', (value) => {
-            socket.in(room).emit('textarea', value);
+        socket.emit('text', textareaHistory)
+        socket.on('text', text => {
+            textareaHistory = text
+            socket.in(room).emit('text', text)
         });
     });
-    socket.on('join-terminal', (room) => {
+
+    socket.on('join-terminal', room => {
+        socket.leaveAll();
+        socket.join(room);
+
+        socket.emit('terminal-history', terminalHistory)
+
         if (!terminalPtys[room]) {
             const ptyProcess = pty.spawn(this.command, this.args || [], {
-                name: 'xterm-color',
-                env: process.env
-            });
-            ptyProcess.onData((output) => {
-                socket.in(room).emit('stdo', output);
-            });
-            terminalPtys[room] = ptyProcess;
-        }
-        else {
-            terminalPtys[room].write('clear\n');
-        }
-        socket.leaveAll();
-        socket.join(room);
-        console.log(socket.id, 'joining terminal room', room);
+            name: 'xterm-color',
+            env: process.env
+        });
 
-        socket.on('stdin', (stdin) => {
-            terminalPtys[room].write(stdin);
+        terminalPtys[room] = ptyProcess;
+
+        ptyProcess.onData(data => {
+            terminalHistory += data
+            socket.in(room).emit('stdout', data)
+        });
+    }
+
+        socket.on('stdin', data => {
+            terminalPtys[room].write(data)
         })
     });
-    socket.on('disconnect', async (reason) => {
+    
+    socket.on('disconnect', async => {
         socket.leaveAll();
         socket.removeAllListeners();
-        console.log(socket.id, 'leaving all rooms');
     })
 });
 
